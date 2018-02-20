f5d7ffb069ea983348bca356b3406d48d1661a20
prashantprabhune <35997998+prashantprabhune@users.noreply.github.com>
2018-02-20 16:16:59 +0530

    Update test_forms.py

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -16,12 +16,3 @@ class RenewBookFormTest(TestCase):
         form_data = {'renewal_date': date}
         form = RenewBookForm(data=form_data)
         self.assertFalse(form.is_valid())
-
-    def test_renew_form_date_too_far_in_future(self):
-        """
-        Test form is invalid if renewal_date more than 4 weeks from today
-        """
-        date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
-        form_data = {'renewal_date': date}
-        form = RenewBookForm(data=form_data)
-        self.assertFalse(form.is_valid())
